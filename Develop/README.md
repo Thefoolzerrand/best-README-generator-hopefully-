7be4a1e5805937460f7d2f35801aa25c6a91c2f9
Byron <dasecondpsalm23@gmail.com>
2025-05-07 13:21:52 -0700

    best read me ever

---
+++ Develop/README.md
@@ -0,0 +1,39 @@
+# byrons project
+
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+  ## Description
+
+  a project made by byron for byron dont touch unless your name is byron
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Tests](#tests)
+  - [License](#license)
+  - [Questions](#questions)
+
+  <h2 id="installation">Installation</h2>
+  npm i
+
+  <h2 id="usage">Usage</h2>
+  npm use XD
+
+  <h2 id="contribution">Contribution</h2>
+  git push hopefully
+
+  <h2 id="tests">Tests</h2>
+  git test
+
+  <h2 id="license">License</h2>
+  licensed under the <a href="https://opensource.org/licenses/MIT">MIT</a> license.
+
+  <h2 id="questions">Questions</h2>
+  
+  If you have questions you can send a message to 
+  the Github account: TheFoolzErrand
+  or 
+  email: Dasecondpsalm23@gmail.com
+
---
+++ Develop/index.js
@@ -0,0 +1,76 @@
+import inquirer from "inquirer";
+import generateMarkdown from "./utils/generateMarkdown.js";
+import fs from 'fs';
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message:'What is the title of your project?',
+    },
+    {
+        type:'input',
+        name:'description',
+        message:'What is a description of the project?',
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message:'How does one install your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message:'How does one use your project?',
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message:'How does one contribute to your project?',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message:'How does one test your project?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message:'What license does your project use?',
+        choices: ['MIT','ISC','Apache','GNU'],
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message:'What is your GitHub user name?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message:'What is the your email address?',
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName,data, (err)=> {
+        if(err){
+            console.error(err);
+        }
+    })    
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((data) => {
+            const fileName = 'README.md'
+            const fileData = generateMarkdown(data);
+            writeToFile(fileName,fileData); 
+        });
+}
+
+// Function call to initialize app
+init();
